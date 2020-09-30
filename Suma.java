7a7d6940a3be23757cb1094192f63f55a5f321de
Virginia Campayo <virginiacampayo.alu@iespacomolla.es>
2020-09-30 10:33:10 +0200

    finish proyectoInicialGIT.

---
+++ Suma.java
@@ -0,0 +1,14 @@
+public class Suma {
+     public static void main(String[ ] args) {
+
+      
+        int num1, num2, rest;
+       
+       num1= 5;
+       num2= num1*num1*num1;
+       rest=num1+num2;
+
+ 
+       System.out.println("El resultado es de "+rest);
+     }               
+} 
